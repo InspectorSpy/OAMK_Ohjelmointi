a4ef6dd03936426f0f74e28a01453fa5da0b7628
InspectorSpy <103323455+InspectorSpy@users.noreply.github.com>
2026-01-11 01:29:15 +0200

    Implement object-oriented guessing game with Game class and header file

---
+++ OOP/Viikkotehtävä 2/Game.cpp
@@ -0,0 +1,25 @@
+/*
+1. Toteuta viikkotehtävän 1 mukainen arvauspeli nyt oliopohjaisesti. 
+Toteuta alla olevan luokkakaavion mukainen luokka Game
+Konstruktorissa parametrinä annetaan maksimiarvo, mille välille 1-maxNumber peli arpoo arvattavan luvun.
+Play()-jäsenfunktioon toteutetaan peli
+printGameResult()-jäsenfunktiosta kutsutaan kun peli on päättynyt. Se tulostaa oikean vastauksen ja kuinka monta arvausta siihen tarvittiin.
+Luokan toteutuksessa täytyy olla molemmat otsikkotiedosto Game.h ja luokan toteutus Game.cpp
+
+2. Lisää peliin debug-tietoa, tulostamalla kontruktorista ja jäsenfunktioista mitä niissä tapahtuu. Esimerkki alla kuvassa. 
+
+3. Lopuksi vielä toteuta ohjelmaan main.cpp-kooditiedosto, missä main()-funktiossa luodaan Game-olio ja ajetaan peli. 
+
+*/
+
+#include "Game.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+Game::Game(int maxNumber) {
+    maxNumber = maxNum;
+    numOfGuesses = 0;
+}
