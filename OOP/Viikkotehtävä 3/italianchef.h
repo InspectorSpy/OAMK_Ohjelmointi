7f96a59222577abfc0b7aa316c9935b1ffbea8df
InspectorSpy <103323455+InspectorSpy@users.noreply.github.com>
2026-01-19 11:05:20 +0200

    Update viikkotehtävä 3;

--- OOP/Viikkotehtävä 3/italianchef.h
+++ OOP/Viikkotehtävä 3/italianchef.h
@@ -7,7 +7,8 @@ Keitto: yhteen annokseen tarvitaan 3 ainesta
 4. Toteuta luokka Chef niin, että jäsenfunktiot, konstruktori ja destruktori tulostavat debug-tietoa. 
 5. Toteuta luokka ItalianChef, joka perii Chef-luokan. 
 6. Lisää luokkaan luokkakaavion mukaiset jäsenmuuttujat. Salasanalle annetaan alustusarvoksi pizza. 
-7. Jäsenfunktio askSecret() tarkistaa onko parametrinä annettu salasana ok, ja jos on, kutsuu makePizza()-jäsenfunktiota. Muut parametrit ovat ainesten eli jauhojen (flour) ja veden (water) määrä. Funktio palauttaa true/false sen mukaan oliko salasana oikea. 
+7. Jäsenfunktio askSecret() tarkistaa onko parametrinä annettu salasana ok, ja jos on, kutsuu makePizza()-jäsenfunktiota.
+Muut parametrit ovat ainesten eli jauhojen (flour) ja veden (water) määrä. Funktio palauttaa true/false sen mukaan oliko salasana oikea. 
 8. makePizza tekee pizzoja niin paljon kuin aineksi riittää, yhteen pizzaan tarvitaan:
 5 ainesta jauhoja
 5 ainesta vettä
