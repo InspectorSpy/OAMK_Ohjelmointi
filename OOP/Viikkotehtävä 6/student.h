941d352dd90ced6e85ca0570e93d19db14ef76d7
InspectorSpy <103323455+InspectorSpy@users.noreply.github.com>
2026-02-15 18:57:35 +0200

    Update viikkotehtävä 6;

--- OOP/Viikkotehtävä 6/student.h
+++ OOP/Viikkotehtävä 6/student.h
@@ -1,3 +1,4 @@
+
 /*
 1. Toteuta C++ projektiisi oheisen UML luokkadiagrammin mukainen  Student luokka.
 2. Saat tehtävän oheistiedostona kuvan mukaisen pääohjelmatiedoston. Kopioi siitä tehtävät C++ projektiisi main.cpp tiedostoon ja tee case 0,1,2,3 ja 4 kohtiin määritetyt tehtävät.
