09c5413665ab271f9eb43f446f6d09f071b7ce8b
InspectorSpy <103323455+InspectorSpy@users.noreply.github.com>
2026-02-18 17:39:12 +0200

    Create viikkotehtävä 7;

---
+++ OOP/Viikkotehtävä 7/main.cpp
@@ -0,0 +1,28 @@
+/*
+1. Toteuta yksikertainen Qt Widged applikaatio, jossa on alla olevan kuvan mukainen käyttöliittymä. Count nappia painettaessa LineEdit olion numero kasvaa ja Reset nappia painettaessa se nollaantuu.
+2. Käyttöliittymässä Number1, Number2 ja Result ovat Qlabel olioita, joita käytetään pelkkinä teksti kenttinä. Eli ne eivät vaadi mitään koodia.
+3. Laatikot, joissa on numerot 1,2 ja 3 ovat QlineEdit olioita ja ne kannattaisi nimetä num1, num2 ja result olioiksi.
+4. Kaikki loput käyttöliittymän laatikot ovat QPushButton olioita. Numero oliot kannatta nimetä N0,N1, N2, N9 ja operaatio oliot add, sub, mul, div nimisiksi ja lopulta clear ja enter oliot.
+
+Vihjeitä:
+1. Aloita tekemällä käyttöliittymän kaikki elementit, nimeä ne ehdotetulla tavalla.
+2. Tee kaikille QPushButton olioille click handler samaan tyyliin kuin tehtävässä 1.
+3. Tee mainwindow.h tiedostoon yllä esitetyt private muuttujat ja metodit.
+4. Laita jokaiseen numeronäppäimen click handleriin kutsu numberClickHandler() funktioon. Eli kaikki numeronäppäimet käsitellään samalla tavalla. Yritä saada ensin toimimaan numeroiden vastaanottaminen näppäimien avulla alla olevan esimerkin mukaan (esimerkissä on ensin painettu numeroa 1 ja sitten numeroa 2. Ohjelma kirjoittaa qDebug() funktion avulla Qstring muuttujien number1 ja number2 muuttujen arvot jokaisen numeronäppäin painalluksen jälkeen). Tähän löytyy esimerkkikoodeja. 
+5. +,-,* tai / näppäin tallentaa operand muuttujaan numeron 0,1,2,3 sen mukaan mitä operaatiota oli painettu ja vaihtaa state = 1 => state = 2, jolloin numeronäppäimet ”keräävät” merkkejä number2 muuttujaan.
+6. Clear näppäin asettaa state = 1, number1 =””, number2=”” ja nollaa QLineEdit ikkunat.
+7. Enter näppäin muuttaa number1 ja number2 Qstring arvot float arvoiksi ja tekee operand muuttujan mukaisen laskutoimituksen ja tallentaa tuloksen result muuttujaan, jonka tulos kirjoitetaan tietysti QLineEdit ikkunaan.
+*/
+
+#include "mainwindow.h"
+#include <QApplication>
+
+using namepsace std;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
